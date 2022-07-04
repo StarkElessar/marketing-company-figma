6a37b6f138973197cdae9954abbd85ecbdca6b22
Stark <serj.elessar@gmail.com>
2022-07-04 17:32:02 +0300

    Add file SWIPER.scss

--- src/js/app.js
+++ src/js/app.js
@@ -1,8 +1,17 @@
 import * as flsFunctions from './modules/functions.js'
+import Swiper, { Navigation } from 'swiper'
 
 flsFunctions.isWebp()
 flsFunctions.setBurgerMenu('.burger-menu', '.menu')
 
-import Swiper, { Navigation, Pagination } from 'swiper'
-
-const swiper = new Swiper()
+const ourClientSwiper = new Swiper('.swiper', {
+  modules: [ Navigation ],
+  direction: 'horizontal',
+  loop: true,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  speed: 400,
+  spaceBetween: 100,
+})
